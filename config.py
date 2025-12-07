ea73531c07874098f963155ae993eac115138ad3
ROHIT12-bit <reddyrohit823@gmail.com>
2025-12-07 13:15:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,9 +2,10 @@ API_ID = 26047636
 API_HASH = "d8b1ed69ae1f937c5dd4d3cc8c8de440" 
 BOT_TOKEN = "8539037971:AAGaWpcn1IpKBnpUIqVeircwhLnJTmwGEco"
 MONGO_URI = "mongodb+srv://rohitreddyathuru:R6Co7MOjTYQOAqcq@cluster0.xrwjpl9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-START_PIC = "https://telegra.ph/file/c39602ab8e3d5932a91d1.jpg" 
-START_MSG = "<blockquote> Hello! I am Sequence Bot 🤖</blockquote>\n\n<blockquote>I can help you organize, sequence, and manage your files effortlessly. Send me your files, and I’ll take care of the rest!</blockquote>\n\n<blockquote>[Powered by Botskingdoms](https://t.me/BOTSKINGDOMS)</blockquote>"
+START_PIC = "https://i.rj1.dev/ADADe.png" 
+START_MSG = "<blockquote> Hello! I am Sequence Bot 🤖</blockquote>\n\n<blockquote>I can help you organize, sequence, and manage your files effortlessly. Send me your files, and I’ll take care of the rest!</blockquote>\n\n<blockquote>[ᴘᴏᴡᴇʀᴇᴅ ʙʏ ʙᴏᴛsᴋɪɴɢᴅᴏᴍs](https://t.me/BOTSKINGDOMS)</blockquote>"
 HELP_TXT = "<b><i>About Us..\n\n‣ Made for : Anime x Hunters\n‣ Owner : @Its_Sahil_Ansari\n‣ Worked For : @Anime_x_hunters</i></b>"
 OWNER_ID = 8367080346
+
 
 

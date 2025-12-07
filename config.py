2d407bfb5b0fd0996a2db9f3a17bf24bb019f75a
ROHIT12-bit <reddyrohit823@gmail.com>
2025-12-07 12:31:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,9 @@
-API_ID = 20421248 
-API_HASH = "90da70b17365b3709b8a0346a7749ce7" 
-BOT_TOKEN = "0"
-MONGO_URI = "0"
+API_ID = 26047636 
+API_HASH = "d8b1ed69ae1f937c5dd4d3cc8c8de440" 
+BOT_TOKEN = "8539037971:AAGaWpcn1IpKBnpUIqVeircwhLnJTmwGEco"
+MONGO_URI = "mongodb+srv://rohitreddyathuru:R6Co7MOjTYQOAqcq@cluster0.xrwjpl9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 START_PIC = "https://telegra.ph/file/c39602ab8e3d5932a91d1.jpg" 
 START_MSG = "<b>Hi There...! \n\nI am a File Sequence Bot.\nI can sequence your files effortlessly.\n\nMy Owner: @Its_Sahil_Ansari</b>"
 HELP_TXT = "<b><i>About Us..\n\n‣ Made for : Anime x Hunters\n‣ Owner : @Its_Sahil_Ansari\n‣ Worked For : @Anime_x_hunters</i></b>"
-OWNER_ID = 5090651635
+OWNER_ID = 8367080346
+

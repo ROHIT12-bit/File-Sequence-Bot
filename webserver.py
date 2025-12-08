492fd7af1d20dea6f4f17e99f924dc49a34a31e6
ROHIT12-bit <reddyrohit823@gmail.com>
2025-12-08 13:42:43 +0530

    Update webserver.py

--- webserver.py
+++ webserver.py
@@ -1,18 +1,12 @@
-import os
-import asyncio
 from flask import Flask
-from sequence import start_bot  # import your bot starter function
+import os
 
-web = Flask(__name__)
+app = Flask(__name__)
 
-@web.route("/")
+@app.route('/')
 def home():
     return "Bot is running!"
 
 if __name__ == "__main__":
-    loop = asyncio.get_event_loop()
-    loop.create_task(start_bot())  # start bot in background
-
-    # Run Flask server on Render port
     port = int(os.environ.get("PORT", 10000))
-    web.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port)
